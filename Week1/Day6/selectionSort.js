112f42896d32f09638a277f448bf863a3bb7620b
Mayis-Mazin <mayis.mazin@gmail.com>
2021-01-06 18:54:28 +0200

    100 DAY OF Code DAY6: Selection Sort

---
+++ Week1/Day6/selectionSort.js
@@ -0,0 +1,22 @@
+function selectionSort(array) { 
+    let n = array.length;
+        
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        for(let j = i+1; j < n; j++){
+            if(array[j] < array[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             // Swapping the elements
+             let tmp = array[i]; 
+             array[i] = array[min];
+             array[min] = tmp;      
+        }
+    }
+    return array;
+}
+
+selectionSort([5,4,6,2]);
