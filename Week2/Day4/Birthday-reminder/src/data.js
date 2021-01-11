bb210c9d80f6f1e598b6cf2fc4e359ece3923f22
Mayis-Mazin <mayis.mazin@gmail.com>
2021-01-11 18:46:04 +0200

    100 DAY OF Code WEEk2:3 DAY11: Birthday-reminder

---
+++ Week2/Day4/Birthday-reminder/src/data.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+ 
+  {
+    id: 3,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
