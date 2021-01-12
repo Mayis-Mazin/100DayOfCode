cf825d496191711c9cd79eb70fc0e54d463678b5
Mayis-Mazin <mayis.mazin@gmail.com>
2021-01-12 06:14:40 +0200

    100 DAY OF Code WEEk2:5 DAY12: Random Quote machine | creat the Button component

---
+++ Week2/Day5/random-quote-machine/src/App.js
@@ -0,0 +1,17 @@
+import React,{ Component } from "react";
+import './App.css';
+import Button from './components/Button'
+class App extends Component {
+nextQuoteClickHandler(){
+  console.log('hi');
+}
+
+render(){
+  return (
+    <div className="quote-box" id="quote-box">
+    <Button buttonDisplayName='Next Quote' clickHandler={this.nextQuoteClickHandler}/>
+    </div>
+  );
+}
+}
+export default App;
---
+++ Week2/Day5/random-quote-machine/src/components/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Button =({buttonDisplayName,clickHandler})=>(
+    <button onClick={clickHandler}>{buttonDisplayName}</button>
+)
+export default Button;
