e2f15d91f6219cb9b46e850923642f1de84a7192
Mayis-Mazin <mayis.mazin@gmail.com>
2021-01-16 23:36:37 +0200

    100 DAY OF Code WEEK3:2 DAY16 Pomodoro Clock | Reset Button

--- Week3/Day2/pomodoroClock/src/App.js
+++ Week3/Day2/pomodoroClock/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState } from "react";
+import React, {useState,useEffect} from "react";
 import Break from './components/Break'
 import Session from './components/Session'
 import TimeLeft from './components/TimeLeft'
@@ -7,7 +7,18 @@ import TimeLeft from './components/TimeLeft'
 
 
 function App() {
+
+  const [currentSessionType,setCurrentSessionType]=useState('Session');//'Session' or 'Break'
+  const[intervalId,setIntervalId]=useState(null)
   const [sessionLength,setSessionLength]=useState(60*25);
+  const[timeLeft,setTimeLeft]=useState(sessionLength)
+
+
+  //change timeLeft whenever sessionLength changes
+  useEffect(()=>{
+      setTimeLeft(sessionLength);
+  },[sessionLength])
+
 
   const decrementSessionLengthByOneMinutes=()=>{
       const newSessionLength=sessionLength-60
@@ -42,6 +53,67 @@ function App() {
   }
 
 
+  const isStarted=intervalId !==null
+  const handleStartStopClick=()=>{
+      if(isStarted){
+  // if we are in the started mode:
+  // we want to stop the timer
+  //clearInterval
+  clearInterval(intervalId)
+  setIntervalId(null)
+
+      }else{
+      //if we are in the stopped mode
+      //decrement timeLeft by one every second (1000 ms)
+      //using setInterval
+      const newIntervalId=setInterval(()=>{
+          setTimeLeft(prevTimeLeft=>{
+             const newTimeLeft=prevTimeLeft-1;
+             if(newTimeLeft>=0){
+                 return prevTimeLeft-1
+             }
+
+             // if session:
+             if(currentSessionType ==='Session'){
+                 //switch to break
+                 setCurrentSessionType('Break')
+                 //setTimeLeft to breakLength
+                 setTimeLeft(breakLength)
+             }
+
+              // if break:
+              if(currentSessionType ==='Break'){
+                  //switch to Session
+                  setCurrentSessionType('Session')
+                  //setTimeLeft to SessionBreak
+                  setTimeLeft(sessionLength)
+              }
+
+
+          })
+      },1000)
+  setIntervalId(newIntervalId)
+      }
+   };
+
+
+
+const handleResetButtonClick=()=>{
+  // clear the timeout interval
+clearInterval(intervalId)
+  // set the intervalId null
+setIntervalId(null)
+  //set the sessionType to 'Session'
+  setCurrentSessionType('Session')
+  //reset the session length to 25 minutes
+  setSessionLength(60*25)
+  //reset the break length to 5 minutes
+  setBreakLength(60*25)
+  //reset the timer to 25 minutes (initial session length)
+  setTimeLeft(60*25)
+}
+
+
   return (
    <div className='App'>
    <Break
@@ -49,12 +121,22 @@ function App() {
      decrementBreakLengthByOneMinutes={decrementBreakLengthByOneMinutes}
      incrementBreakLengthByOneMinutes={incrementBreakLengthByOneMinutes}
    />
-   <TimeLeft breakLength={breakLength} sessionLength={sessionLength}/>
+   <TimeLeft 
+   handleStartStopClick={handleStartStopClick}
+   breakLength={breakLength}
+    timerLabel={currentSessionType}
+     sessionLength={sessionLength}
+     timeLeft={timeLeft}
+       startStopButtonLabel={isStarted?'Stop':'Start'}
+     />
    <Session
      sessionLength={sessionLength}
      incrementSessionLengthByOneMinutes={incrementSessionLengthByOneMinutes}
      decrementSessionLengthByOneMinutes={decrementSessionLengthByOneMinutes}
    />
+   <button onClick={handleResetButtonClick}>
+     Reset
+   </button>
     </div>
   );
 }
--- Week3/Day2/pomodoroClock/src/components/TimeLeft.jsx
+++ Week3/Day2/pomodoroClock/src/components/TimeLeft.jsx
@@ -1,72 +1,21 @@
-import React,{useState, useEffect} from 'react'
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format'
 
 momentDurationFormatSetup(moment)
 const TimeLeft=(
-   {breakLength, sessionLength}
+   {
+    handleStartStopClick,
+    timerLabel,
+    startStopButtonLabel,
+    timeLeft}
 )=>{
-    const [currentSessionType,setCurrentSessionType]=useState('Session');//'Session' or 'Break'
-    const[intervalId,setIntervalId]=useState(null)
-    const[timeLeft,setTimeLeft]=useState(sessionLength)
-    const formattedTimeLeft=moment.duration(timeLeft,'s').format('mm:ss',{trim:false})
-    //change timeLeft whenever sessionLength changes
-    useEffect(()=>{
-        setTimeLeft(sessionLength);
-    },[sessionLength])
-
-    const isStarted=intervalId !==null
-    const handleStartStopClick=()=>{
-        if(isStarted){
-    // if we are in the started mode:
-    // we want to stop the timer
-    //clearInterval
-    clearInterval(intervalId)
-    setIntervalId(null)
-
-        }else{
-        //if we are in the stopped mode
-        //decrement timeLeft by one every second (1000 ms)
-        //using setInterval
-        const newIntervalId=setInterval(()=>{
-            setTimeLeft(prevTimeLeft=>{
-               const newTimeLeft=prevTimeLeft-1;
-               if(newTimeLeft>=0){
-                   return prevTimeLeft-1
-               }
-
-               // if session:
-               if(currentSessionType ==='Session'){
-                   //switch to break
-                   setCurrentSessionType('Break')
-                   //setTimeLeft to breakLength
-                   setTimeLeft(breakLength)
-               }
-
-                // if break:
-                if(currentSessionType ==='Break'){
-                    //switch to Session
-                    setCurrentSessionType('Session')
-                    //setTimeLeft to SessionBreak
-                    setTimeLeft(sessionLength)
-                }
-
-
-            })
-        },1000)
-    setIntervalId(newIntervalId)
-        }
-     };
-
-  
-
-
+  const formattedTimeLeft=moment.duration(timeLeft,'s').format('mm:ss',{trim:false})
    
     return (
         <div>
-        <p id='timer-label'>{currentSessionType}</p>
+        <p id='timer-label'>{timerLabel}</p>
         <p id='time-left'>{formattedTimeLeft}</p>
-        <button onClick={handleStartStopClick}>{isStarted?'Stop':'Start'}</button>
+        <button onClick={handleStartStopClick}>{startStopButtonLabel}</button>
         </div>
     )
 }
