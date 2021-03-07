dcd163d88f3bec0bdd8231e118ccfa8b8d60fde7
Mayis-Mazin <mayis.mazin@gmail.com>
2021-03-07 21:14:13 +0200

    WEEK9:7 DAY63: Debounce

---
+++ Week10/Day3/sumRange.js
@@ -0,0 +1,26 @@
+// get the sum of a range of numbers
+function range(start, end, step) {
+    const arr = []
+    if (step < 0) {
+      for (var i = start; i >= end; acum) {
+        arr.push(i)
+        var acum = typeof step !== 'undefined' ? (i += step) : i++
+      }
+    } else {
+      var acum = 0
+      for (var i = start; i <= end; acum) {
+        arr.push(i)
+        var acum = typeof step !== 'undefined' ? (i += step) : i++
+      }
+    }
+    return arr
+  }
+  
+  function sum(arr) {
+    console.log(arr)
+    let count = 0
+    for (const element of arr) {
+      count += element
+    }
+    return count
+  }
