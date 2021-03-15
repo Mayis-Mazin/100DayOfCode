8f9d2cc7e0acb04fa3e67a1b0e5330a353fc2951
Mayis-Mazin <mayis.mazin@gmail.com>
2021-03-15 18:47:27 +0200

    Week11:4 Day:74: bytes To Friendly Size

---
+++ Week11/Day4/bytesToFriendlySize.js
@@ -0,0 +1,32 @@
+// convert long numbers to friendly, readable sizes
+
+function bytesToSize(bytes) {
+    const size = ["Bytes", "KB", "MB", "GB", "TB"];
+    if (bytes === 0) {
+      return "0 Bytes";
+    }
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes;
+  }
+  
+  function formatBytes(bytes, decimals) {
+    if (bytes === 0) {
+      return "0 Bytes";
+    }
+    const k = 1000;
+    const dm = decimals + 1 || 3;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(dm) + " " + sizes[i];
+  }
+  
+  function convertBytes(input, precision) {
+    const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let index = 0;
+    const factor = Math.pow(10, precision > 0 ? precision : 2);
+  
+    while (input >= 1024 && suffixes[++index]) {
+      input /= 1024;
+    }
+    return Math.round(input * factor) / factor + " " + suffixes[index];
+  }
