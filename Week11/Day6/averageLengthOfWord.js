4221027058258b95ca8e49d17779b30f4176e2a7
Mayis-Mazin <mayis.mazin@gmail.com>
2021-03-17 20:40:35 +0200

    Week11:6 Day:76 Average Length Word.

---
+++ Week11/Day6/averageLengthOfWord.js
@@ -0,0 +1,12 @@
+// find the average length of each word in a string
+
+const lengthOfString = function (myString) {
+    const words = myString.split(' ')
+    let average = 0
+    let total = 0
+    for (const element of words) {
+      total += element.length
+    }
+    average = total / words.length
+    return average
+  }
