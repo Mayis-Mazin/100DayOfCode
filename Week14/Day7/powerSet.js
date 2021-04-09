9754d5e59442d9f34031db28bf9e537e96e209b2
Mayis-Mazin <mayis.mazin@gmail.com>
2021-04-09 11:28:26 +0300

    WEEK14:7 DAY98: Power Set

---
+++ Week14/Day7/powerSet.js
@@ -0,0 +1,30 @@
+/*
+ * Return an array with the power set of a given string.
+ *
+ * Example:
+ *
+ * powerSet("abc")
+ * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
+ *
+ * Note: result does need to be sorted or in any particular order, it only needs to contain
+ * the correct set of values.
+ */
+
+var powerSet = function (str) {
+    if (str.length === 0) return [''];
+    if (str.length === 1) return [ str ];
+  
+    var result = [];
+    var first  = str[0];
+    var rest   = str.slice(1);
+    var recur  = powerSet( rest );
+  
+    recur.forEach( function (s) {
+      result.push( s );
+      result.push( first + s );
+    });
+  
+    result.push( first );
+  
+    return result;
+  };
